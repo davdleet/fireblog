76c4b5581814617ea5ce5551b3c89d858d2e56db
davdleet <davdleet1@gmail.com>
2022-08-10 17:41:18 +0900

    added user context

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,7 +1,7 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
-import 'firebase/firestore'
-import 'firebase/storage'
+import 'firebase/compat/firestore'
+import 'firebase/compat/storage'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -21,5 +21,33 @@ if (!firebase.apps.length) {
 }
 
 export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
+
+/*
+    Gets a users/{uid} document with username
+*/
+
+export async function getUserWithUsername(username) {
+    const usersRef = firestore.collection('users');
+    const query = usersRef.where('username', '==', username).limit(1);
+    const userDoc = (await query.get()).docs[0];
+    return userDoc;
+}
+
+/**`
+ * Converts a firestore document to JSON
+ * @param  {DocumentSnapshot} doc
+ */
+export function postToJSON(doc) {
+    const data = doc.data();
+    return {
+        ...data,
+        // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
+        createdAt: data.createdAt.toMillis(),
+        updatedAt: data.updatedAt.toMillis(),
+    };
+}
+
+export const fromMillis = firebase.firestore.Timestamp.fromMillis;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar'
+import { Toaster } from 'react-hot-toast'
+import { UserContext } from '../lib/context'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { UseUserData } from '../lib/hooks'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const userData = UseUserData();
+  return (
+    <UserContext.Provider value={userData}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
+    </UserContext.Provider>
+  )
 }
 
 export default MyApp
