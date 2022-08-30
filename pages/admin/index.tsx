cf617be56d00aa86fa9b961c78e9a5174c87f3c3
davdleet <davdleet1@gmail.com>
2022-08-30 20:00:08 +0900

    posts lists for user and write new post

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,6 +1,85 @@
+
+import Head from 'next/head';
+import AuthCheck from '../../components/AuthCheck';
+import PostFeed from '../../components/PostFeed';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { UserContext } from '../../lib/context';
+import { firestore, auth, serverTimestamp } from '../../lib/firebase';
+import { useRouter } from 'next/router';
+import { useContext, useState } from 'react';
+import kebabCase from 'lodash.kebabcase';
+import toast from 'react-hot-toast';
+import styles from '../../styles/Admin.module.css';
 export default function AdminPostsPage({ }) {
     return (
         <main>
+            <AuthCheck>
+                <PostList />
+                <CreateNewPost />
+            </AuthCheck>
         </main>
     )
+}
+
+function PostList() {
+    const ref = firestore.collection('users').doc(auth.currentUser.uid).collection('posts');
+    const query = ref.orderBy('createdAt');
+    const [querySnapshot] = useCollection(query);
+    const posts = querySnapshot?.docs.map((doc) => doc.data());
+
+    return (
+        <>
+            <h1>Manage your posts</h1>
+            <PostFeed posts={posts} admin />
+        </>
+    )
+}
+
+function CreateNewPost() {
+    const router = useRouter();
+    const { username } = useContext(UserContext);
+    const [title, setTitle] = useState('');
+
+    const slug = encodeURI(kebabCase(title));
+
+    const isValid = title.length > 3 && title.length < 100;
+
+    const createPost = async (e) => {
+        e.preventDefault();
+        const uid = auth.currentUser.uid;
+        const ref = firestore.collection('users').doc(uid).collection('posts').doc(slug);
+
+        const data = {
+            title,
+            slug,
+            uid,
+            username,
+            published: false,
+            content: '# hello world!',
+            createdAt: serverTimestamp(),
+            updatedAt: serverTimestamp(),
+            heartCount: 0,
+        };
+
+        await ref.set(data);
+
+        toast.success('Post created!');
+
+        router.push(`/admin/${slug}`);
+    };
+
+    return (
+        <form onSubmit={createPost}>
+            <input
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Enter a title for your post"
+                className={styles.input}
+            />
+            <p>
+                <strong>Slug:</strong> {slug}
+            </p>
+            <button type="submit" disabled={!isValid} className="btn-green">Create New Post</button>
+        </form >
+    );
 }
