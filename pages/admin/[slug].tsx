5a7f552f763ad1f58850551dd4b10d60360e395f
davdleet <davdleet1@gmail.com>
2022-09-20 22:49:41 +0900

    error message and disable submit button

--- pages/admin/[slug].tsx
+++ pages/admin/[slug].tsx
@@ -53,8 +53,9 @@ function PostManager() {
 }
 
 function PostForm({ defaultValues, postRef, preview }) {
-    const { register, handleSubmit, reset, watch } = useForm({ defaultValues, mode: 'onChange' });
+    const { register, handleSubmit, reset, watch, formState: { errors } } = useForm({ defaultValues, mode: 'onChange' });
 
+    //const { isValid, isDirty } = formState;
     const updatePost = async ({ content, published }) => {
         await postRef.update({
             content,
@@ -77,14 +78,18 @@ function PostForm({ defaultValues, postRef, preview }) {
 
             <div className={preview ? styles.hidden : styles.controls}>
 
-                <textarea name="content" {...register('content')}></textarea>
-
+                <textarea name="content" {...register('content', {
+                    maxLength: { value: 20000, message: 'content is too long' },
+                    minLength: { value: 10, message: 'content is too short' },
+                    required: { value: true, message: 'content is required' }
+                })}></textarea>
+                {errors.content && <p className="text-danger">{errors.content.message}</p>}
                 <fieldset>
                     <input className={styles.checkbox} name="published" type="checkbox" {...register('published')} />
                     <label>Published</label>
                 </fieldset>
 
-                <button type="submit" className="btn-green">
+                <button type="submit" className="btn-green" disabled={errors.content}>
                     Save Changes
                 </button>
             </div>
