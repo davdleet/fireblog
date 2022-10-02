c94b282355a98a8cc69ff16d15ed17849ae956ad
davdleet <davdleet1@gmail.com>
2022-10-02 19:56:36 +0900

    production heart button

--- components/HeartButton.tsx
+++ components/HeartButton.tsx
@@ -3,11 +3,8 @@ import { useDocument } from 'react-firebase-hooks/firestore';
 import { doc, collection, writeBatch } from 'firebase/firestore';
 export default function Heart({ postRef }) {
     // listen to heart document for current user
-    console.log(postRef.path);
-    const heartRef = doc(firestore, 'hearts', auth.currentUser.uid) //postRef.collection('hearts').doc(auth.currentUser.uid);
+    const heartRef = doc(firestore, postRef.path, 'hearts', auth.currentUser.uid) //postRef.collection('hearts').doc(auth.currentUser.uid);
     const [heartDoc] = useDocument(heartRef);
-
-
     // create a user-to-post relationship
     const addHeart = async () => {
         const uid = auth.currentUser.uid;
